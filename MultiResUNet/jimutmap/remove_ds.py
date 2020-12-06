635fe034bdcf5c8fa62c43f19a1ae63eafa9e536
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2020-12-06 12:29:44 +0530

    remove ds

---
+++ MultiResUNet/jimutmap/remove_ds.py
@@ -0,0 +1,19 @@
+import glob
+images_list = sorted(glob.glob("jimutmap_less/map/*"))
+masks_list = sorted(glob.glob("jimutmap_less/roads/*"))
+print(images_list[:10])
+print(len(images_list))
+print(len(masks_list))
+print(masks_list[:10])
+print(len(masks_list)- len(masks_list)*0.5)
+import os
+"""
+from tqdm import tqdm
+
+for img_fl in tqdm(images_list[:58852]):
+    os.remove(img_fl)
+    #print(img_fl)
+    mask_name = "jimutmap_less/roads/"+str(img_fl.split('.')[0]).split('/')[-1]+"_road.png"
+    os.remove(mask_name)
+"""
+
